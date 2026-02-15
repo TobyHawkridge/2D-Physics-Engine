710c6ce4bd8269b08eb42b4c6217490b615522dc
TobyHawkridge <hawkridgetoby@gmail.com>
2026-02-15 01:23:12 +0000

    Add: movement to shapes

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ This project is a 2D physics engine being made in the coding language 'C'.
 
 RayLib is needed to run this project currrently, this may change in the future. The RayLib installer is on 'https://raylib.com' (it asks for a donation but you can just click to the actual download with the 'No thanks, just take me to the downloads' button). Make sure you really want RayLib installed because the installer is 647mb and the lib is 1.79gb.
 
-The project is currently a black screen popup that opens auto maximized and fullscreen. To exit the program, press esc or manually close it with the x button in the top right. To toggle black and white background, hold and let go of the backspace. Held down is a white background and not held is a black background. When you hold the 's' key, a square with an opposite colour to the background (e.g. black background but white square) and you can hold the 'c' key to do the same but for a circle.
+The project is currently a black screen popup that opens auto maximized and fullscreen. To exit the program, press esc or manually close it with the x button in the top right. To toggle black and white background, hold and let go of the backspace. Held down is a white background and not held is a black background. When you hold the 's' key, a square with an opposite colour to the background (e.g. black background but white square) and you can hold the 'c' key to do the same but for a circle. Use the arrow keys whilst the shapes are on to move that particular shape. 
 
 To use the project, run main.exe.
+
+(Note to coders. I'm using deltaTime logic, am I cool? 😊)
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
+    SetTargetFPS(60);
     const int screenWidth = 1600;
     const int screenHeight = 1200;
 
@@ -16,13 +17,18 @@ int main(void)
     }
 
     Color bg = WHITE;
-    int square_pos_x = 400;
-    int square_pos_y = 400;
-    int circle_pos_x = 400;
-    int circle_pos_y = 400;
+    float square_pos_x = 400.0f;
+    float square_pos_y = 400.0f;
+    float circle_pos_x = 400.0f;
+    float circle_pos_y = 400.0f;
+    float speed = 800.0f;
+    bool S = false;
+    bool C = false;
 
     while (!WindowShouldClose())
     {
+        float deltaTime = GetFrameTime();
+
         if (IsKeyPressed(KEY_F11))
         {
             ToggleFullscreen();
@@ -43,9 +49,58 @@ int main(void)
         
         // DRAWING A SQUARE
         if (IsKeyDown(KEY_S))
+        {
+            S = true;
+
             DrawRectangle(square_pos_x, square_pos_y, 200, 200, bg);
+
+            if (IsKeyDown(KEY_UP) && S == true)
+            {
+                square_pos_y -= speed * deltaTime;
+            }
+            if (IsKeyDown(KEY_DOWN) && S == true)
+            {
+                square_pos_y += speed * deltaTime;
+            }
+            if (IsKeyDown(KEY_RIGHT) && S == true)
+            {
+                square_pos_x += speed * deltaTime;
+            }
+            if (IsKeyDown(KEY_LEFT) && S == true)
+            {
+                square_pos_x -= speed * deltaTime;
+            }
+            
+        }
+        else
+            S = false;
+
         if (IsKeyDown(KEY_C))
+        {
+            C = true;
+
             DrawCircle(circle_pos_x, circle_pos_y, 200, bg);
+
+            if (IsKeyDown(KEY_UP) && C == true)
+            {
+                circle_pos_y -= speed * deltaTime;
+            }
+            if (IsKeyDown(KEY_DOWN) && C == true)
+            {
+                circle_pos_y += speed * deltaTime;
+            }
+            if (IsKeyDown(KEY_RIGHT) && C == true)
+            {
+                circle_pos_x += speed * deltaTime;
+            }
+            if (IsKeyDown(KEY_LEFT) && C == true)
+            {
+                circle_pos_x -= speed * deltaTime;
+            }
+            
+        }
+        else
+            C = false;
 
         EndDrawing();
     }
